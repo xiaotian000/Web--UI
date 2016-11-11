d916fa7492175d9b74a8f41bba892393e83038a7
小TIAN.★☆ <1756768668@qq.com>
2016-11-11 18:31:27 +0800

    页面2

---
+++ 页面2/js/yemian.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    $('a').hover(
+    function () {
+        $(this).stop().animate({"width":"100px"},200);},
+    function(){
+        $(this).stop().animate({"width":"100px"},200);},
+    function(){
+        $(this).stop().animate({"width":"100px"},200);}
+    )
+})
+$(document).ready(function () {
+    $('a').mouseleave(function () {
+        $(this).stop().animate({ "width": "80px" }, 200);}
+    )
+})
