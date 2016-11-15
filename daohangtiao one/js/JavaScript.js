5b4e35b03c18b5c6df88d9d6395968684b836024
小TIAN.★☆ <1756768668@qq.com>
2016-11-15 17:33:27 +0800

    导航条

--- daohangtiao one/js/JavaScript.js
+++ daohangtiao one/js/JavaScript.js
@@ -1,16 +1,17 @@
-﻿$(function () {
+﻿$(document).ready(function () {
     var index = window.location.href.split("/").length - 1;
     var href = window.location.href.split("/")[index];
-    $(".nav nav-list li a[href=' " + href + " ']").addClass("on");
-
-    var li_width = $(".nav  .nav-list li a.on").outerwidth();
-    var li_left = $(".nav ..nav-list li a.on").position().left;
+    $(".nav .nav-list li a[href='" + href + "']").addClass("on");
+    var $li_a = $(".nav .nav-list li a.on");
+    var li_width = $li_a.outerWidth();
+    var li_left = $li_a.position().left;
     $(".nav-content .nav-line").css({ width: li_width, left: li_left });
-
     $(".nav .nav-list li").hover(function () {
-        var li_width = $(this).outerwidth();
+        var li_width = $(this).outerWidth();
         var li_left = $(this).position().left;
+        console.log(li_width + ',' + li_left);
         $(".nav-content .nav-line").stop().animate({ "width": li_width, "left": li_left }, { duration: 1500, easing: "easeOutElastic" });
-        $(".nav -content.nav-line").stop().animate({ "width": li_width, "left": lli_left }, { duration: 1500, easing: "easeOutElastic" });
+    }, function () {
+        $(".nav-content .nav-line").stop().animate({ "width": li_width, "left": li_left }, { duration: 1500, easing: "easeOutElastic" });
     });
 });
