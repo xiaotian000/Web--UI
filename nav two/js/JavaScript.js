8f3bb26eb0641797a330f5558bc4e859e0c0d908
小TIAN.★☆ <1756768668@qq.com>
2016-11-22 16:58:05 +0800

    nav two-jquery

---
+++ nav two/js/JavaScript.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $(".nav-list li a").hover(function () {
+        $(this).stop().animate({ "margin-top": -40 }, 200)
+    }, function () {
+        $(this).stop().animate({ "margin-top": 0 }, 200)
+    });
+});
