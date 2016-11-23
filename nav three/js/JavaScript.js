2ad4fe37734a81a567493424fcbc3958a8518d43
小TIAN.★☆ <1756768668@qq.com>
2016-11-23 16:35:26 +0800

    nav three-jquery

---
+++ nav three/js/JavaScript.js
@@ -0,0 +1,8 @@
+﻿
+$(function () {
+    $(".nav .nav-list li").hover(function () {
+        $(this).find(".nav-down").stop().slideDown()
+    }, function () {
+        $(this).find(".nav-down").stop().slideUp()
+    });
+});
