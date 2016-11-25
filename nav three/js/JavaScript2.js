53454442587d888797c670c1083499f8bead6caa
小TIAN.★☆ <1756768668@qq.com>
2016-11-25 16:09:02 +0800

    nav-jquery.easying

---
+++ nav three/js/JavaScript2.js
@@ -0,0 +1,7 @@
+﻿$(function(){ 
+$(".nav .nav-list li").hover(function(){ 
+$(this).find(".nav-down").stop().slideDown({duration:500,easing:"easeOutBounce"}) 
+},function(){ 
+$(this).find(".nav-down").stop().slideUp({duration:500,easing:"easeOutBounce"}) 
+}); 
+});
