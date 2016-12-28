3a6c0514711a8914f9e915809db65fa0e35f7c2c
小TIAN.★☆ <1756768668@qq.com>
2016-12-28 17:59:39 +0800

    Microsoft

--- Microsoft/js/JavaScript.js
+++ Microsoft/js/JavaScript.js
@@ -1,10 +1,10 @@
 ﻿$(function () {
-    var page = 1;
-    var i = 3;
+    var page = 1;//初始化page变量；
+    var i = 3;//每版放3组图片；
     var $sliderShow = $(".slider");
-    var downwidth = $sliderShow.width();
-    var len = $(".silde").find('.item').length;
-    var page_number = Math.ceil(len / i);
+    var downwidth = $sliderShow.width();//获取框架宽度，即每次移动的距离；
+    var len = $(".silde").find('.item').length;//找到一共几组图片；
+    var page_number = Math.ceil(len / i);//找到一共几组版面；
     //for (var i = 0; i < len; i++) {
     //    var item = $(".silde").find('.item')[i];
     //    if (i!=0) {
@@ -12,25 +12,25 @@
     //    }
 
     //}
-    $("#carousel-left").click(function () {
-        if (!$(".silde").is(":animated")) {
-            if (page == page_number) {
-                $(".silde").animate({ left: '+=' + downwidth * (page_number - 1) }, "slow");
+    $("#carousel-left").click(function () { //点击左侧箭头；
+        if (!$(".silde").is(":animated")) { //判断是否正在执行动画效果；
+            if (page == page_number) { //已经到达最后一个版面，应该跳转到第一个版面；
+                $(".silde").animate({ left: '+=' + downwidth * (page_number - 1) }, "slow"); //动画效果向左移动，改变left的值，跳转到第一个版面；
                 page = 1;
             } else {
-                $(".silde").animate({ left: '-=' + downwidth }, "slow");
+                $(".silde").animate({ left: '-=' + downwidth }, "slow"); //动画效果向左移动，依次改变left的值，到达每一个版面；
                 page++;
             }
         }
         return false;
     });
-    $("#carousel-right").click(function () {
-        if (!$(".silde").is(":animated")) {
-            if (page == page_number) {
-                $(".silde").animate({ left: "+=" + downwidth * (page_number - 1) }, "slow");
+    $("#carousel-right").click(function () { //点击右侧箭头；
+        if (!$(".silde").is(":animated")) { //判断是否在执行动画效果；
+            if (page == page_number) { //已经到达最后一个版面，应该跳到第一个版面；
+                $(".silde").animate({ left: "+=" + downwidth * (page_number - 1) }, "slow"); //动画效果向右移动，改变left的值，移动到第一个版面；
                 page = 1;
             } else {
-                $(".silde").animate({ left: '-=' + downwidth }, "slow");
+                $(".silde").animate({ left: '-=' + downwidth }, "slow");//动画依次向左移动，到达每一个版面；
                 page++;
             }
         }
