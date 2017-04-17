c2012c884d7887d86fb2f935c6a1231ffe12dd9a
小TIAN.★☆ <1756768668@qq.com>
2017-04-17 17:44:10 +0800

    vertical

---
+++ vertical/js/JavaScript.js
@@ -0,0 +1,20 @@
+﻿
+$(function () {
+    var winHeight = $(document).scrollTop();
+
+    $(window).scroll(function () {
+        var scrollY = $(document).scrollTop();
+        if (scrollY > 550) {
+            $('.fixed_nav').addClass('hiddened');
+        }
+        else {
+            $('.fixed_nav').removeClass('hiddened');
+        }
+        if (scrollY > winHeight) {
+            $('.fixed_nav').removeClass('showed');
+        }
+        else {
+            $('.fixed_nav').addClass('showed');
+        }
+    });
+});
