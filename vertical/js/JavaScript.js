4816375d8f185a567f98212872b174964c7f3ac6
小TIAN.★☆ <1756768668@qq.com>
2017-04-20 16:48:34 +0800

    vertical

--- vertical/js/JavaScript.js
+++ vertical/js/JavaScript.js
@@ -1,9 +1,13 @@
-﻿$(function () {
-    this.fixed_nav = function () {
-        if ($(document).scrollTop() > 150) {
-            $("#fixed_nav").slideDown(300);
-        } else {
-            $("#fixed_nav").fadeOut(100);
-        }
+﻿function fixed_nav() {
+    if ($(document).scrollTop() > 150) {
+        $("#fixed_nav").slideDown(300);
+    } else {
+        $("#fixed_nav").fadeOut(100);
     }
-}); 
+}
+
+$(function () {
+    $(document).scroll(function () {
+        fixed_nav();
+    })
+})
