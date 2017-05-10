44ef90e9e10a04b78d2222013d962866fbbfedfc
小TIAN.★☆ <1756768668@qq.com>
2017-05-10 14:21:09 +0800

    login

--- login/javascript/loginjs.js
+++ login/javascript/loginjs.js
@@ -2,4 +2,10 @@
     $(".button").click(function () {
         location.href = 'http://www.baidu.com';
     });
+    $(".controls").click(function () {
+        $("input").attr('checked');
+    });
+    $(".controls").change(function () {
+        $("input").css();
+    });
 })
