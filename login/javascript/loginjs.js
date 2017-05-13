526450a8432b6239a473f0e12d8e6078cda8398a
小TIAN.★☆ <1756768668@qq.com>
2017-05-13 23:42:38 +0800

    login

--- login/javascript/loginjs.js
+++ login/javascript/loginjs.js
@@ -3,5 +3,8 @@
         location.href = 'http://www.baidu.com';
     });
     $(".controls").click(function () {
+        $("input").change(function () {
+            $("em").css("background-position", "url(https://www.lgstatic.com/lp/static/images/check_8486fe6.png) left -12px no-repeat;");
+        });       
     });
 })
